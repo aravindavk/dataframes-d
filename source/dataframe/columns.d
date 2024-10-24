7e8a5765cf85b221fe08ed6977e0a04ccd34cf2c
Aravinda VK <vkaravinda7@gmail.com>
2024-10-24 13:45:54 +0530

    Add column datatype and DataFrame initialization
    Also added the code comments
    
    Signed-off-by: Aravinda VK <vkaravinda7@gmail.com>

---
+++ source/dataframe/columns.d
@@ -0,0 +1,24 @@
+module dataframe.columns;
+
+/**
+ * DataFrame Column to store all the elements of
+ * the column.
+ */
+struct Column(T)
+{
+    T[] data;
+
+    /**
+     * Get the length of Items in the column
+     */
+    size_t length()
+    {
+        return data.length;
+    }
+}
+
+unittest
+{
+    auto data = Column!double([100.0, 200.0, 300.0]);
+    assert(data.length == 3);
+}
--- source/dataframe/package.d
+++ source/dataframe/package.d
@@ -2,14 +2,56 @@ module dataframe;
 
 import std.traits;
 
+import dataframe.columns;
+
+
+/**
+ * DataFrame
+ */
 struct DataFrame(T)
 {
     alias fieldNames = FieldNameTuple!(T);
     alias fieldTypes = FieldTypeTuple!(T);
 
+    static foreach(idx, type; fieldTypes)
+    {
+        mixin(Column!(type).stringof ~ " " ~ fieldNames[idx] ~ ";");
+    }
+
+    /**
+     * Get the list of column names
+     */
+    string[] columnNames()
+    {
+        string[] names;
+        foreach(name; fieldNames)
+            names ~= name;
+
+        return names;
+    }
+
+    /**
+     * Get the number of columns
+     */
     size_t ncol()
     {
         return fieldNames.length;
+    }
+
+    /**
+     * Get the number of rows
+     */
+    size_t length()
+    {
+        return  __traits(getMember, this, fieldNames[0]).length;
+    }
+
+    /**
+     * Get the number of rows. Alias of `length`
+     */
+    size_t nrow()
+    {
+        return length;
     }
 }
 
@@ -25,4 +67,13 @@ unittest
     DataFrame!Item df;
 
     assert(df.ncol == 3);
+
+    df.name = Column!string(["A", "B", "C", "D"]);
+    df.price = Column!double([149.0, 799.0, 399.0, 299.0]);
+    df.quantity = Column!int([3, 1, 2, 1]);
+
+    assert(df.nrow == 4);
+    assert(df.length == 4);
+
+    assert(df.columnNames == ["name", "price", "quantity"]);
 }
